9ed3ab84c0b6538f772fb45e8efaf6f12eb0860c
Han Castanedo <hancastanedo@gmail.com>
2018-09-21 15:35:34 -0400

    fix append innerWidth

--- js/desktop.js
+++ js/desktop.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  if (window.width > 479) {
+  if (window.innerWidth > 479) {
  $('#ftr').append('<script src="js/delay-scroll-test.js"></script>');
  $('#ftr').append('<script src="js/jquery-test.js"></script>');
 }
