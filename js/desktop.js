ea6bed0f661b916c0b919667ebfe3e9e6274c761
Han Castanedo <hancastanedo@gmail.com>
2018-09-26 13:42:32 -0400

    add rules and device svg window

--- js/desktop.js
+++ js/desktop.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  if (window.innerWidth > 479) {
+  if (window.innerWidth > 800) {
  $('#ftr').append('<script src="js/delay-scroll-test.js"></script>');
  $('#ftr').append('<script src="js/jquery-test.js"></script>');
 }
