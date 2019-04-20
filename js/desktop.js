fd2bc23883584beb29935ab6282c536b3fa47aad
Han Castanedo <han@Hans-MacBook-Air.local>
2019-04-20 08:30:47 -0400

    remove delay scroll from css

--- js/desktop.js
+++ js/desktop.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   if (window.innerWidth > 479) {
- $('#ftr').append('<script src="js/delay-scroll-test.js"></script>');
+// $('#ftr').append('<script src="js/delay-scroll-test.js"></script>');
  $('#ftr').append('<script src="js/jquery-test.js"></script>');
 }
 });
